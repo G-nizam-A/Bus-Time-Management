8998c0f0ba8dfd71e9f536cdf9e30ca5ddf69840
G-nizam-A <nizamcs10@gmail.com>
2022-09-21 17:13:44 +0530

    First Commit

---
+++ src/Project/ConnectionProvider.java
@@ -0,0 +1,15 @@
+package Project;
+import java.sql.*;
+
+public class ConnectionProvider {
+    public static Connection getCon(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bus","root","root");
+            return con;
+        }
+        catch(Exception e){
+            return null;
+        }
+    }
+}
